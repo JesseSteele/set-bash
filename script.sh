3574af49418dd1efca54df75b78b901724b14ec4
Jesse Steele <JesselSteele@users.noreply.github.com>
2016-09-26 15:47:45 +0800

    Add the files via upload

---
+++ script.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+. ~/set-bash/settings-file
+
+echo "$MY_FIRST_VARIABLE"
+
+# I like {curly brackets}
+## And I like to make notes for myself in Markdown
+echo "${MY_SECOND_VARIABLE}"
+
+# Below is expirimental and could break the new server
+echo "$MY_THIRD_VARIABLE"
